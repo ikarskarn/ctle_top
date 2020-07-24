dbcb3e877cc2a9d9dc4e5e09c9591982029a4b70
Marc Tucker <marcttucker@gmail.com>
2020-07-24 19:24:10 -0400

    updated names and urls

---
+++ precache-manifest.d96cdf842fe20962d46e2c5b2b73d284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa85c2278186f7fcd3fa3383f09b762",
+    "url": "/ctle_top/index.html"
+  },
+  {
+    "revision": "1de54e3eb5462977c4c5",
+    "url": "/ctle_top/static/css/main.acbe5e33.chunk.css"
+  },
+  {
+    "revision": "5bed94f0bc8e62ceb715",
+    "url": "/ctle_top/static/js/2.825ec038.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ctle_top/static/js/2.825ec038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de54e3eb5462977c4c5",
+    "url": "/ctle_top/static/js/main.0231103e.chunk.js"
+  },
+  {
+    "revision": "9f105a5613891ed89a77",
+    "url": "/ctle_top/static/js/runtime-main.0885fe2c.js"
+  }
+]);
