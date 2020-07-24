4bb15ce773324606b32e1f682e9ff6c2c59645fd
Marc Tucker <marcttucker@gmail.com>
2020-07-24 13:14:51 -0400

    test 4 of parent page open

--- src/components/ProgramsAndEvents_Content/Page_ProgramsAndEvents.js
+++ src/components/ProgramsAndEvents_Content/Page_ProgramsAndEvents.js
@@ -48,8 +48,8 @@ export default class ProgramsAndEvents extends React.Component {
                   reflect
                 </li>
               </ul>
-              <base target="_parent"><a href="https://docs.google.com/document/d/1KMS6rAMMMy1iI9V_M43kIJfPoNf2sDzyzTziKylGLx8/edit?usp=sharing" target="_blank"> CTLE 3 Learning Programs, Descriptions, and Guidelines
-              </a></base>
+              <base target="_parent"/><a href="https://docs.google.com/document/d/1KMS6rAMMMy1iI9V_M43kIJfPoNf2sDzyzTziKylGLx8/edit?usp=sharing" target="_blank"> CTLE 3 Learning Programs, Descriptions, and Guidelines
+              </a>
             </section>
           </div>
         </div>
