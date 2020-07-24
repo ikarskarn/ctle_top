f792c5620bf67b658c7609caec659ed6131e1bd2
Marc Tucker <marcttucker@gmail.com>
2020-07-24 13:09:34 -0400

    test 3 of page load

--- precache-manifest.4b5d22c1632755fbb929552ece5e3c36.js
+++ precache-manifest.4b5d22c1632755fbb929552ece5e3c36.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a34a9827cc6dedbf99d3af647cfdfe96",
+    "revision": "0d2bc41ad8b00ded89b43855904b4366",
     "url": "/ctle_top/index.html"
   },
   {
-    "revision": "a34f8839ca8962ea2926",
+    "revision": "58586cae231622646dd3",
     "url": "/ctle_top/static/css/main.8c212cd1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ctle_top/static/js/2.122c4f17.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a34f8839ca8962ea2926",
-    "url": "/ctle_top/static/js/main.d23bc6d4.chunk.js"
+    "revision": "58586cae231622646dd3",
+    "url": "/ctle_top/static/js/main.31ef9fd1.chunk.js"
   },
   {
     "revision": "9f105a5613891ed89a77",
