ec8f3b54e2a18461472d9acd74bb325f2e7f29b9
Marc Tucker <marcttucker@gmail.com>
2020-07-27 00:31:56 -0400

    added faculty admin duties page

---
+++ precache-manifest.d6f3f1149c28d7664b289d4e50765257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5d6a8ca285152760907311dcde823e",
+    "url": "/ctle_top/index.html"
+  },
+  {
+    "revision": "c34810f7db9b37785b19",
+    "url": "/ctle_top/static/css/main.87b0b42f.chunk.css"
+  },
+  {
+    "revision": "95e13088ce27227aaf0d",
+    "url": "/ctle_top/static/js/2.d6b39ef7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ctle_top/static/js/2.d6b39ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34810f7db9b37785b19",
+    "url": "/ctle_top/static/js/main.159dff48.chunk.js"
+  },
+  {
+    "revision": "9f105a5613891ed89a77",
+    "url": "/ctle_top/static/js/runtime-main.0885fe2c.js"
+  }
+]);
