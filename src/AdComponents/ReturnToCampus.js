ed2c5c026fe58a94c2d10941ebba40ba27b8bb01
Marc Tucker <marcttucker@gmail.com>
2020-08-07 09:00:47 -0400

    fixed bad link on return to campus ad

--- src/AdComponents/ReturnToCampus.js
+++ src/AdComponents/ReturnToCampus.js
@@ -9,7 +9,7 @@ export default function ReturnToCampus(props) {
                 </h3>
                 <p>If you are returning to campus, here is what you need to know</p>
                 <base target='_parent'/>
-                <a href='https://inside.cpcc.edu/initiatives/return-to-campus-plan-covid-19/22813_covid19_employeereturn_print1.pdfSeptember' target='_blank' rel='noopener noreferrer' className='link b-white b-shadow'>PDF Guide</a>
+                <a href='https://inside.cpcc.edu/initiatives/return-to-campus-plan-covid-19/22813_covid19_employeereturn_print1.pdf' target='_blank' rel='noopener noreferrer' className='link b-white b-shadow'>PDF Guide</a>
             </div>
         </div>
     );
