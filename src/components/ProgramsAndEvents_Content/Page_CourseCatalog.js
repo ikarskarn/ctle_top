441d7dae5bbce068ff1e2277e365c9b5bf4ab235
Marc Tucker <marcttucker@gmail.com>
2020-08-12 14:10:53 -0400

    pushed a minor update to correct deployment

--- src/components/ProgramsAndEvents_Content/Page_CourseCatalog.js
+++ src/components/ProgramsAndEvents_Content/Page_CourseCatalog.js
@@ -6,11 +6,7 @@ export default class Page_CourseCatalog extends React.Component {
     render() {
         return (
             <div className="page">
-                {/*<header className="b-grey">
-                    <h2>
-                        Course Catalog
-                    </h2>
-                </header>*/}
+                
                 <section className='list-section b-white'>
                     <p className='paragraph-emphasis'>
                         Expand your professional practice, learning, and development in the Teaching Excellence Framework (Faculty Competencies). In this catalog, you will find a variety of learning tracks, certifications, and learning activities are available in support of student learning and success. Can’t find a training or workshop of interest? Let us know.
